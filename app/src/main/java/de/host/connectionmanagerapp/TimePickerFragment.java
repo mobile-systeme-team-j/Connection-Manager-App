ab67af79fc03d564a17199353ff41e6ad263ac17
Mattis <lord.blackbro@gmail.com>
2019-05-08 21:00:42 +0200

    androidx migriert

--- app/src/main/java/de/host/connectionmanagerapp/TimePickerFragment.java
+++ app/src/main/java/de/host/connectionmanagerapp/TimePickerFragment.java
@@ -8,7 +8,7 @@ import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
+import androidx.fragment.app.DialogFragment;
 import java.util.Calendar;
 
 // Zur Anzeige der Zeit in Cornjob/Alarm - Activity
