fdbdb11100639609a0fe634ecd0bed00ccc9893b
BL4CK4RD <lord.blackbro@gmail.com>
2019-05-09 21:33:51 +0200

    Database builder

--- app/src/main/java/de/host/connectionmanagerapp/Database.java
+++ app/src/main/java/de/host/connectionmanagerapp/Database.java
@@ -1,9 +1,27 @@
 package de.host.connectionmanagerapp;
 
+import android.content.Context;
+
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @androidx.room.Database(entities = {Connection.class,Identity.class,Snippet.class}, version = 1)
 public abstract class Database extends RoomDatabase {
 
 
+    private static Database INSTANCE;
+
+    static Database getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (Database.class) {
+                if (INSTANCE == null) {
+                    INSTANCE =
+                            Room.databaseBuilder(context.getApplicationContext(),
+                                    Database.class,
+                                    "database").build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
