0ee6826d1c3b16576efd3f1a977254b08f03e115
itsDaVen <t.manuel97@yahoo.com>
2019-05-14 16:27:41 +0200

    MainActivity + container.xml dienen nun als Container für Fragments erste Navigation möglich

--- app/src/main/java/de/host/connectionmanagerapp/MainActivity.java
+++ app/src/main/java/de/host/connectionmanagerapp/MainActivity.java
@@ -1,48 +1,54 @@
 package de.host.connectionmanagerapp;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.view.Menu;
-import android.view.MenuItem;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * @author Manuel Trapp
+ * @date 14.05.2019
+ * */
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout side_menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.container);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        side_menu = findViewById(R.id.layout_main);
+        side_menu = findViewById(R.id.layout_container);
+        NavigationView navigationView= findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, side_menu, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         side_menu.addDrawerListener(toggle);
         toggle.syncState();
 
-      /*  FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-       */
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+        }
+
     }
 
-    // handles side-menu when Back-Button of Phone is pressed
+
     @Override
     public void onBackPressed(){
+        // handles side-menu when Back-Button of Phone is pressed
         if(side_menu.isDrawerOpen(GravityCompat.START)){
             side_menu.closeDrawer(GravityCompat.START);
         } else {
@@ -56,18 +62,42 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+
+
+
+
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        // handles selected menu option
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(menuItem.getItemId()){
+            case R.id.menu_home:
+                   getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+                break;
+            case R.id.menu_wizard:
+                //   getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ConnectionsFragment()).commit();
+                break;
+            case R.id.menu_connections:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ConnectionsFragment()).commit();
+                break;
+            case R.id.menu_identities:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new IdentitiesFragment()).commit();
+                break;
+            case R.id.menu_snippets:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SnippetsFragment()).commit();
+                break;
+          /*  case R.id.menu_job:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new JobsFragment()).commit();
+                break;
+            case R.id.menu_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SnippetsFragment()).commit();
+                break;
+            case R.id.menu_about:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SnippetsFragment()).commit();
+                break;
+           */
         }
-
-        return super.onOptionsItemSelected(item);
+        side_menu.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
