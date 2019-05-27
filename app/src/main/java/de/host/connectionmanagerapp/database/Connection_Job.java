ea3ab88817dcfc532fd93904e808014b4e58e0a7
BL4CK4RD <lord.blackbro@gmail.com>
2019-05-27 12:19:22 +0200

    connection job wieder da

---
+++ app/src/main/java/de/host/connectionmanagerapp/database/Connection_Job.java
@@ -0,0 +1,38 @@
+package de.host.connectionmanagerapp.database;
+
+
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Index;
+
+@Entity(tableName = "connection_job_join",
+        primaryKeys = { "connection_Id", "job_Id" },
+        indices = {@Index(value={"job_Id"})},
+        foreignKeys = {
+
+                @ForeignKey(entity = Connection.class,
+                        parentColumns = "connection_Id",
+                        childColumns = "connection_Id"),
+                @ForeignKey(entity = Job.class,
+                        parentColumns = "job_Id",
+                        childColumns = "job_Id")
+        })
+public class Connection_Job {
+
+    private  final long job_Id;
+    private  final long connection_Id;
+
+
+    public Connection_Job(long job_Id, long connection_Id) {
+        this.job_Id = job_Id;
+        this.connection_Id = connection_Id;
+    }
+
+    public long getJob_Id() {
+        return job_Id;
+    }
+
+    public long getConnection_Id() {
+        return connection_Id;
+    }
+}
