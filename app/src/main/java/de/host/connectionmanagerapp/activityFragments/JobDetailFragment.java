c255b0820a672fd69c27ddc3c68f7a6c28b0adab
bl4ckOut <phillip.kuehling@web.de>
2019-06-17 17:08:40 +0200

    DatePicker and TimePicker in JobDetail works now

--- app/src/main/java/de/host/connectionmanagerapp/activityFragments/JobDetailFragment.java
+++ app/src/main/java/de/host/connectionmanagerapp/activityFragments/JobDetailFragment.java
@@ -1,21 +1,17 @@
 package de.host.connectionmanagerapp.activityFragments;
 
-import android.app.DatePickerDialog;
-import android.app.TimePickerDialog;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
-import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -37,16 +33,19 @@ import de.host.connectionmanagerapp.viewmodels.ConnectionViewModel;
 
 public class JobDetailFragment extends Fragment
         implements View.OnClickListener,
-        AdapterView.OnItemSelectedListener,
-                    TimePickerDialog.OnTimeSetListener,
-                    DatePickerDialog.OnDateSetListener {
+        AdapterView.OnItemSelectedListener{
 
     private DatePickerFragment datePickerFragment;
+    private TimePickerFragment timePickerFragment;
+    public static final int REQUEST_CODE_DatePicker = 11; // Used to identify the date result
+    public static final int REQUEST_CODE_TimePicker = 22; // Used to identify the time result
     EditText editTextDate;
     EditText editTextTime;
     EditText editTextJobName;
     Job job;
     Bundle arguments;
+    private String selectedDate;
+    private String selectedTime;
     long id;
     private ConnectionViewModel connectionViewModel;
     Spinner spinnerConnection,spinnerIdentity, spinnerSnippet;
@@ -66,11 +65,13 @@ public class JobDetailFragment extends Fragment
         connectionViewModel= ViewModelProviders.of(getActivity()).get(ConnectionViewModel.class);
 
         view.findViewById(R.id.job_name);
+        datePickerFragment = new DatePickerFragment();
+        timePickerFragment = new TimePickerFragment();
 
          View Buttons[]={
-                 view.findViewById(R.id.date),
-                 view.findViewById(R.id.time),
-                 view.findViewById(R.id.job_name),
+                 editTextDate = view.findViewById(R.id.date),
+                 editTextTime = view.findViewById(R.id.time),
+                 editTextJobName = view.findViewById(R.id.job_name),
                  view.findViewById(R.id.btnConnection),
                  view.findViewById(R.id.btnIdentity),
                  view.findViewById(R.id.btnSnippet),
@@ -162,25 +163,37 @@ public class JobDetailFragment extends Fragment
         return job;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // check for the results
+        if (requestCode == REQUEST_CODE_DatePicker && resultCode == Activity.RESULT_OK) {
+            // get date from string
+            selectedDate = data.getStringExtra("selectedDate");
+            // set the value of the editText
+            editTextDate.setText(selectedDate);
+        }
+
+        if (requestCode == REQUEST_CODE_TimePicker && resultCode == Activity.RESULT_OK) {
+            // get date from string
+            selectedTime = data.getStringExtra("selectedTime");
+            // set the value of the editText
+            editTextTime.setText(selectedTime);
+        }
+
+    }
 
     public void DatePicker(){
-        DialogFragment datePicker = new DatePickerFragment();
-        datePicker.show(getFragmentManager(), "DatePickerFragment.TAG");
+        // set the targetFragment to receive the results, specifying the request code
+        datePickerFragment.setTargetFragment(JobDetailFragment.this, REQUEST_CODE_DatePicker);
+        // show the datePicker
+        datePickerFragment.show(getFragmentManager(), "datePicker");
     }
 
     public void TimePicker(){
-          DialogFragment timePicker = new TimePickerFragment();
-          timePicker.show(getFragmentManager(), "time picker");
-    }
-
-    @Override
-    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        editTextTime.setText(hourOfDay + ":" + minute);
-    }
-
-    @Override
-    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-        editTextDate.setText(dayOfMonth+"-"+month+"-"+year);
+        // set the targetFragment to receive the results, specifying the request code
+        timePickerFragment.setTargetFragment(JobDetailFragment.this, REQUEST_CODE_TimePicker);
+        // show the datePicker
+        timePickerFragment.show(getFragmentManager(), "timePicker");
     }
 
     @Override
